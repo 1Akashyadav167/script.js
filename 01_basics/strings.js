baf5f3e11c3680a153f029296535f2d39f52a19d
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-10 12:04:55 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,25 @@
+const name="Bruce wyne";
+const age =32;
+console.log(name+age)
+console.log(`My Name is${name} and my Age is${age}`);
+
+let num=new String("Bruce")
+console.log(num);
+console.log(num[0]);
+console.log(num.__proto__);
+console.log(num.toUpperCase());
+
+console.log(num.charAt(2));
+console.log(num.indexOf('u'));
+console.log(num.substring(1,4));
+console.log(num.slice(-3,1));
+
+// trim() method remove the starting and ending space
+
+const url="brucewyne@20%gotham"
+console.log(url.replace('20%','--'));
+
+
+
+
+
