5ee69218b66e98c178fce7ade164cdda71472daf
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-11 18:05:29 +0000

    date

---
+++ 01_basics/dates.js
@@ -0,0 +1,12 @@
+// Dates
+
+let dates=new Date()
+console.log(dates.toString())
+console.log(dates.toDateString())
+console.log(dates.toISOString())
+console.log(dates.toJSON())
+console.log(typeof dates)
+
+
+let myDate=Date.now()
+console.log(Math.floor(Date.now()/1000))
