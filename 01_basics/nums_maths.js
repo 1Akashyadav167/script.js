a0ad34eb8462b898f83c64d30c12531e88185571
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-11 11:44:39 +0000

    maths and nums

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,28 @@
+const score=100
+console.log(score);
+
+let newScore=new Number(300)
+console.log(newScore)
+console.log(newScore.toString());
+
+console.log(newScore.toFixed(4));
+
+const decImal=23.856
+console.log(decImal.toPrecision(2))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+//*********Maths***************** */
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.random());  // Math.random always give value between 0 and 1
+console.log((Math.random()*10)+1); // after multiply with it shift by one place and by adding 1 to avoid case of getting zero
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log((Math.random()*(max-min+1))+min) // for gettinf value between max and min
+
+
+
