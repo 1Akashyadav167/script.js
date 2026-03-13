85415288a1082a5899c0125e43c4649a6c21e056
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-13 18:16:47 +0000

    function

--- 01_basics/function2.js
+++ 01_basics/function2.js
@@ -9,7 +9,27 @@
 // console.log(userName())
 
 
-function addCart(item1,item2,...items){  // ... is rest operator it return in array
-    return items
+// function addCart(item1,item2,...items){  // ... is rest operator it return in array
+//     return items
+// }
+// console.log(addCart(44,33,22,11,4444,555,))
+
+
+
+// function and object
+
+function useObj(anyObject){
+    console.log(`the user name is${anyObject.user}and age is${anyObject.age}`)
 }
-console.log(addCart(44,33,22,11,4444,555,))
+useObj({
+    user:"Bruce Wyne ",
+    age:33,
+})
+
+
+//**********Hoisting************ */
+
+sumof(3)
+function sumof(num){
+    return num+1
+}
