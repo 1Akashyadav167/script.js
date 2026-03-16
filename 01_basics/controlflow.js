0e1a1b908d2b19b00dcfd518d7a8072fa432d6fb
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-16 16:23:14 +0000

    if-else

---
+++ 01_basics/controlflow.js
@@ -0,0 +1,23 @@
+// ****if 
+// const useId="Bruce"
+// if(useId=="Bruce"){
+//     console.log("The batman return hero name")
+// }
+
+
+// const user=2
+// if(user==="2"){         // == is check values and === is check value as well as data types
+//     console.log("executed")
+// }
+
+
+
+//**if-else *//
+
+const temperature=40;
+if(temperature==40){
+    console.log("Temperature is greater than 30");
+}
+else{  
+    console.log("Temperature is  not greater than 30")
+}
---
+++ 01_basics/controlflow2.js
@@ -0,0 +1,9 @@
+const power=300;
+if(power>100){
+    var fly= true;
+    console.log(`you can fly is true or false:${fly}`);
+
+}
+console.log(`you can fly is true or false:${fly}`);
+
+/// if there is const or let keyword use the 7 line not execute as it shown the fly is not declare....as var has functional scope
