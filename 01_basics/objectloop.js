ae623ae25e7eb543c41e6ca73b6741219e09a6e0
1Akashyadav167 <theakashyadav2010@gmail.com>
2026-03-17 13:46:16 +0000

    objectloop

---
+++ 01_basics/objectloop.js
@@ -0,0 +1,12 @@
+const lang={
+    js:"javascript",
+    cpp:"C++",
+    html:"Hyper text markup language"
+}
+
+for (const key in lang) {
+    console.log(`${key} is shortcut of${lang[key]} `);
+    
+    
+    
+}
